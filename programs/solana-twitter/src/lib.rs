84fe8dfdac5dfdb6e69fdd24d655733a91054072
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-05-05 01:37:44 +0600

    Added tweet struct and lengths

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -2,6 +2,13 @@ use anchor_lang::prelude::*;
 
 declare_id!("HqsiivaPH3nR31xzsmht43Y1p2y9Vud7hJ9CUSVGnG3t");
 
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const TIMESTAMP_LENGTH: usize = 8;
+const STRING_LENGTH_PREFIX: usize = 4; // Stores the size of the string.
+const MAX_TOPIC_LENGTH: usize = 50 * 4; // 50 chars max.
+const MAX_CONTENT_LENGTH: usize = 280 * 4; // 280 chars max.
+
 #[program]
 pub mod solana_twitter {
     use super::*;
@@ -13,3 +20,17 @@ pub mod solana_twitter {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[account]
+pub struct Tweet {
+    pub author: Pubkey,
+    pub timestamp: i64,
+    pub topic: String,
+    pub content: String,
+}
+
+impl Tweet {
+    const LEN : usize = DISCRIMINATOR_LENGTH + PUBLIC_KEY_LENGTH + TIMESTAMP_LENGTH + STRING_LENGTH_PREFIX + MAX_CONTENT_LENGTH + MAX_CONTENT_LENGTH;
+
+    
+}
