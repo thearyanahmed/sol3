48961ab436698d2059ecafb1a212cfd80d87b703
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-05-05 03:05:27 +0600

    Added ui

---
+++ app/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './main.css'
+
+import { createRouter, createWebHashHistory } from 'vue-router'
+import routes from './routes'
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+})
+
+createApp(App).use(router).mount('#app')
+
---
+++ app/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [],
+  purge: [
+      './public/index.html',
+      './src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
