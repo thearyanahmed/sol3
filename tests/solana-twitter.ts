8d489d7cf16c249a05b759951e6ffde3a731b096
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2022-05-05 02:55:18 +0600

    Added more tests

--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -2,6 +2,7 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { SolanaTwitter } from "../target/types/solana_twitter";
 import * as assert from "assert";
+import * as bs58 from "bs58";
 
 describe("solana-twitter", () => {
   // Configure the client to use the local cluster.
@@ -73,7 +74,7 @@ describe("solana-twitter", () => {
 
         // Call the "SendTweet" instruction on behalf of this other user.
         const tweet = anchor.web3.Keypair.generate();
-        await program.rpc.sendTweet('veganism', 'Yay Tofu!', {
+        await program.rpc.sendTweet('welcome', 'Yay Tofu!', {
             accounts: {
                 tweet: tweet.publicKey,
                 author: otherUser.publicKey,
@@ -87,7 +88,7 @@ describe("solana-twitter", () => {
 
         // Ensure it has the right data.
         assert.equal(tweetAccount.author.toBase58(), otherUser.publicKey.toBase58());
-        assert.equal(tweetAccount.topic, 'veganism');
+        assert.equal(tweetAccount.topic, 'welcome');
         assert.equal(tweetAccount.content, 'Yay Tofu!');
         assert.ok(tweetAccount.timestamp);
     });
@@ -116,7 +117,7 @@ describe("solana-twitter", () => {
         try {
             const tweet = anchor.web3.Keypair.generate();
             const contentWith281Chars = 'x'.repeat(281);
-            await program.rpc.sendTweet('veganism', contentWith281Chars, {
+            await program.rpc.sendTweet('welcome', contentWith281Chars, {
                 accounts: {
                     tweet: tweet.publicKey,
                     author: program.provider.wallet.publicKey,
@@ -130,5 +131,46 @@ describe("solana-twitter", () => {
         }
 
         assert.fail('The instruction should have failed with a 281-character content.');
+    });
+
+    it('can fetch all tweets', async () => {
+        const tweetAccounts = await program.account.tweet.all();
+        assert.equal(tweetAccounts.length, 3);
+    });
+
+    it('can filter tweets by author', async () => {
+        const authorPublicKey = program.provider.wallet.publicKey
+        const tweetAccounts = await program.account.tweet.all([
+            {
+                memcmp: {
+                    offset: 8, // Discriminator.
+                    bytes: authorPublicKey.toBase58(),
+                }
+            }
+        ]);
+
+        assert.equal(tweetAccounts.length, 2);
+        assert.ok(tweetAccounts.every(tweetAccount => {
+            return tweetAccount.account.author.toBase58() === authorPublicKey.toBase58()
+        }))
+    });
+
+    it('can filter tweets by topics', async () => {
+        const tweetAccounts = await program.account.tweet.all([
+            {
+                memcmp: {
+                    offset: 8 + // Discriminator.
+                        32 + // Author public key.
+                        8 + // Timestamp.
+                        4, // Topic string prefix.
+                    bytes: bs58.encode(Buffer.from('welcome')),
+                }
+            }
+        ]);
+
+        assert.equal(tweetAccounts.length, 2);
+        assert.ok(tweetAccounts.every(tweetAccount => {
+            return tweetAccount.account.topic === 'welcome'
+        }))
     });
 });
